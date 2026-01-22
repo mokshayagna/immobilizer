7933c2d97c53ef5be8b6a181b31b02cedf9bba50
mokshayagna <mokshayagna09@gmail.com>
2026-01-22 18:25:42 +0530

    Removed C assignments (moved to separate C repository)

--- Network-Frames/moksha/c_assignments/strings/astr_test.h
+++
@@ -1,7 +0,0 @@
-void test_astrlen(void);
-void test_astrcmp(void);
-void test_astrcat(void);
-void test_astrncat(void);
-void test_astrcpy(void);
-void test_astrncpy(void);
-void test_astrncmp(void);
--- Network-Frames/moksha/c_assignments/strings/astrlen_test.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include "astr.h"
-#include "astr_test.h"
-
-void test_astrlen(void)
-{
-    char s[] = "Hello";
-    printf("astrlen: %d\n", astrlen(s));
-}
